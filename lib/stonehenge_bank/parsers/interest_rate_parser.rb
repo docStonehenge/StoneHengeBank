35e2df5227413946f741acc94a3439ca488ab259
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-04-30 21:50:13 -0300

    Object that parses string to set interest rate

---
+++ lib/stonehenge_bank/parsers/interest_rate_parser.rb
@@ -0,0 +1,21 @@
+require 'stonehenge_bank/interest_rate'
+
+module StonehengeBank
+  module Parsers
+    class InterestRateParser
+      def initialize(rate)
+        @rate = rate.split(/\%\s+/)
+      end
+
+      def construct_interest_rate
+        StonehengeBank::InterestRate.new(rate_value, @rate[1])
+      end
+
+      private
+
+      def rate_value
+        @rate[0].to_f / 100.0
+      end
+    end
+  end
+end
---
+++ spec/stonehenge_bank/parsers/interest_rate_parser_spec.rb
@@ -0,0 +1,19 @@
+require 'stonehenge_bank/parsers/interest_rate_parser'
+
+module StonehengeBank
+  module Parsers
+    describe InterestRateParser do
+      subject { described_class.new('6% monthly') }
+
+      describe '#construct_interest_rate' do
+        it 'returns a new interest rate with value and period' do
+          result = subject.construct_interest_rate
+
+          expect(result).to be_an_instance_of StonehengeBank::InterestRate
+          expect(result.value).to eql(0.06)
+          expect(result.period).to eql('monthly')
+        end
+      end
+    end
+  end
+end
