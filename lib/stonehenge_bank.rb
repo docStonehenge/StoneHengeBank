9d6c03a6e14aed70bb850931c7bdf87656dfa10e
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-05-04 16:55:54 -0300

    First sketch on semester equivalency calculator

--- lib/stonehenge_bank.rb
+++ lib/stonehenge_bank.rb
@@ -5,6 +5,7 @@ require "stonehenge_bank/resources/investment"
 require "stonehenge_bank/calculators/interest_equivalency"
 require "stonehenge_bank/calculators/year_interest_equivalency"
 require 'stonehenge_bank/calculators/month_interest_equivalency'
+require 'stonehenge_bank/calculators/semester_interest_equivalency'
 require "stonehenge_bank/parsers/interest_rate_parser"
 
 module StonehengeBank
---
+++ spec/stonehenge_bank/calculators/semester_interest_equivalency_spec.rb
@@ -0,0 +1,61 @@
+module StonehengeBank
+  module Calculators
+    describe SemesterInterestEquivalency do
+      let(:interest_rate) { double(:interest_rate, value: 0.05) }
+
+      subject { described_class.new(interest_rate) }
+
+      context 'as subclass of InterestEquivalency' do
+        it { is_expected.to respond_to :transformed_rate }
+        it { is_expected.to respond_to :equivalent_rate_power }
+      end
+
+      describe '#equivalent_rate_power' do
+        specify 'when rate period is annually' do
+          allow(interest_rate).to receive(:annually?).and_return true
+
+          expect(subject.equivalent_rate_power).to eql 0.5
+        end
+
+        specify 'when rate period is quarterly' do
+          allow(interest_rate).to receive(:annually?).and_return false
+          allow(interest_rate).to receive(:quarterly?).and_return true
+
+          expect(subject.equivalent_rate_power).to eql 2
+        end
+
+        specify 'when rate period is monthly' do
+          allow(interest_rate).to receive(:annually?).and_return false
+          allow(interest_rate).to receive(:quarterly?).and_return false
+          allow(interest_rate).to receive(:monthly?).and_return true
+
+          expect(subject.equivalent_rate_power).to eql 6
+        end
+
+        specify 'when rate period is daily' do
+          allow(interest_rate).to receive(:annually?).and_return false
+          allow(interest_rate).to receive(:quarterly?).and_return false
+          allow(interest_rate).to receive(:monthly?).and_return false
+          allow(interest_rate).to receive(:daily?).and_return true
+
+          expect(subject.equivalent_rate_power).to eql 180
+        end
+      end
+
+      describe '#transformed_rate' do
+        it 'returns the same value if rate period is semiannual' do
+          expect(interest_rate).to receive(:semiannually?).and_return true
+
+          expect(subject.transformed_rate).to eql 0.05
+        end
+
+        it 'returns a calculated value based on the equivalent rate power' do
+          expect(interest_rate).to receive(:semiannually?).and_return false
+          allow(subject).to receive(:equivalent_rate_power).and_return 6
+
+          expect(subject.transformed_rate).to eql 0.3401
+        end
+      end
+    end
+  end
+end
