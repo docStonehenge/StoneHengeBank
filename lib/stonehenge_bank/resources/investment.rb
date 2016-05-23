eae6d69e0373821733f223262d33df2a93eb71f4
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-05-23 11:18:46 -0300

    Injects equivalency calculator instead of coupling constants

--- lib/stonehenge_bank/resources/investment.rb
+++ lib/stonehenge_bank/resources/investment.rb
@@ -3,30 +3,17 @@ module StonehengeBank
     class Investment
       attr_reader :present_value, :future_value
 
-      def initialize(interest_rate, present_value: nil, future_value: nil)
-        @interest_rate = interest_rate
+      def initialize(present_value: nil, future_value: nil)
         @present_value = present_value
         @future_value  = future_value
       end
 
-      def calculated_future_value(period, quantity)
-        (@present_value * normalize_rate_based_on_period(period, quantity)).round(2)
+      def calculated_future_value(equivalency, quantity)
+        (@present_value * ((1 + equivalency.transformed_rate)**quantity)).round(2)
       end
 
-      def calculated_present_value(period, quantity)
-        (@future_value / normalize_rate_based_on_period(period, quantity)).round(2)
-      end
-
-      private
-
-      def normalize_rate_based_on_period(period, quantity)
-        (1 + generated_period_equivalency(period).transformed_rate)**quantity
-      end
-
-      def generated_period_equivalency(period)
-        StonehengeBank.const_get(
-          "Calculators::#{period.capitalize}InterestEquivalency"
-        ).new(@interest_rate)
+      def calculated_present_value(equivalency, quantity)
+        (@future_value / ((1 + equivalency.transformed_rate)**quantity)).round(2)
       end
     end
   end
--- spec/stonehenge_bank/resources/investment_spec.rb
+++ spec/stonehenge_bank/resources/investment_spec.rb
@@ -5,18 +5,18 @@ module StonehengeBank
       let(:interest_equivalency) { double(:calculator) }
 
       context 'a newly created investment' do
-        subject { described_class.new(interest_rate, present_value: 100.0) }
+        subject { described_class.new(present_value: 100.0) }
 
         it { is_expected.to have_attributes(present_value: 100.0, future_value: nil) }
       end
 
       context 'can be created with a future value instead' do
-        subject { described_class.new(interest_rate, future_value: 100.0) }
+        subject { described_class.new(future_value: 100.0) }
 
         it { is_expected.to have_attributes(present_value: nil, future_value: 100.0) }
       end
 
-      subject { described_class.new(interest_rate, present_value: 100.0) }
+      subject { described_class.new(present_value: 100.0) }
 
       before do
         allow(
@@ -29,15 +29,15 @@ module StonehengeBank
 
       describe '#calculated_future_value period, quantity' do
         it 'returns the calculated future value based on the period asked' do
-          expect(subject.calculated_future_value(:year, 2)).to eql(101.20)
+          expect(subject.calculated_future_value(interest_equivalency, 2)).to eql(101.20)
         end
       end
 
       describe '#calculated_present_value period, quantity' do
-        let(:investment) { Investment.new(interest_rate, future_value: 150.0) }
+        let(:investment) { Investment.new(future_value: 150.0) }
 
         it 'returns the calculated present value based on the period asked' do
-          expect(investment.calculated_present_value(:year, 2)).to eql(148.22)
+          expect(investment.calculated_present_value(interest_equivalency, 2)).to eql(148.22)
         end
       end
     end
