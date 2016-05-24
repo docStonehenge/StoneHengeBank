7c1601ff716182148704629a295b33dc59062eda
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-05-24 10:45:26 -0300

    Sets calculation for investment period

--- lib/stonehenge_bank/resources/investment.rb
+++ lib/stonehenge_bank/resources/investment.rb
@@ -15,6 +15,15 @@ module StonehengeBank
       def calculated_present_value(equivalency, quantity)
         (@future_value / ((1 + equivalency.transformed_rate)**quantity)).round(2)
       end
+
+      def calculated_investment_period(equivalency)
+        unless @present_value && @future_value
+          raise 'Cannot calculate period with null values.'
+        end
+
+        (Math.log(@future_value/@present_value) /
+          Math.log(1 + equivalency.transformed_rate)).ceil
+      end
     end
   end
 end
--- spec/stonehenge_bank/resources/investment_spec.rb
+++ spec/stonehenge_bank/resources/investment_spec.rb
@@ -22,22 +22,58 @@ module StonehengeBank
         allow(
           Calculators::YearInterestEquivalency
         ).to receive(:new).with(interest_rate).and_return(interest_equivalency)
-
-        allow(interest_equivalency).to receive(:transformed_rate).
-                                         and_return(0.006)
       end
 
-      describe '#calculated_future_value period, quantity' do
+      describe '#calculated_future_value equivalency, quantity' do
         it 'returns the calculated future value based on the period asked' do
-          expect(subject.calculated_future_value(interest_equivalency, 2)).to eql(101.20)
+          allow(interest_equivalency).to receive(:transformed_rate).
+                                          and_return(0.006)
+
+          expect(
+            subject.calculated_future_value(interest_equivalency, 2)
+          ).to eql(101.20)
         end
       end
 
-      describe '#calculated_present_value period, quantity' do
+      describe '#calculated_present_value equivalency, quantity' do
         let(:investment) { Investment.new(future_value: 150.0) }
 
         it 'returns the calculated present value based on the period asked' do
-          expect(investment.calculated_present_value(interest_equivalency, 2)).to eql(148.22)
+          allow(interest_equivalency).to receive(:transformed_rate).
+                                          and_return(0.006)
+
+          expect(
+            investment.calculated_present_value(interest_equivalency, 2)
+          ).to eql(148.22)
+        end
+      end
+
+      describe '#calculated_investment_period equivalency' do
+        it 'returns the period in which an investment takes place to a determined value and rate' do
+          investment = Investment.new(present_value: 100.0, future_value: 1450.0)
+
+          allow(interest_equivalency).to receive(:transformed_rate).
+                                          and_return(0.1248)
+
+          expect(
+            investment.calculated_investment_period(interest_equivalency)
+          ).to eql 23
+        end
+
+        it 'returns an error if present value is not set' do
+          investment = Investment.new(future_value: 1450.0)
+
+          expect {
+            investment.calculated_investment_period(interest_equivalency)
+          }.to raise_error('Cannot calculate period with null values.')
+        end
+
+        it 'returns an error if future value is not set' do
+          investment = Investment.new(present_value: 1450.0)
+
+          expect {
+            investment.calculated_investment_period(interest_equivalency)
+          }.to raise_error('Cannot calculate period with null values.')
         end
       end
     end
