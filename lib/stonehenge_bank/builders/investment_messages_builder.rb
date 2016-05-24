23745489625fb1ef16330bb77e403d2c1b6c2a2a
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-05-24 14:01:22 -0300

    Adds formatted message for present value calculation

--- lib/stonehenge_bank/builders/investment_messages_builder.rb
+++ lib/stonehenge_bank/builders/investment_messages_builder.rb
@@ -1,31 +1,48 @@
 module StonehengeBank
   module Builders
     class InvestmentMessagesBuilder
-      CALCULATED_VALUE_MESSAGE = "An investment with %{current_value_type} \
+      InvestmentMessagesBuilderOptions = OpenStruct.new(
+        calculated_value_message: "An investment with %{current_value_type} \
 value of $%{current_value}, an interest rate of %{rate} and a period of \
-%{period_quantity} %{period}(s) returns a %{value_to_reach_type} \
-value of $%{value_to_reach}."
-
-      ERROR_MESSAGE = "Cannot elaborate a message because of an error on \
+%{period_quantity} %{period}(s) %{verb} a %{value_to_reach_type} \
+value of $%{value_to_reach}.",
+        error_message: "Cannot elaborate a message because of an error on \
 the investment calculation: %{error}. Please, check the values needed to \
 be able to call the calculation again."
+      )
 
       def initialize(investment)
         @investment = investment
       end
 
-      def calculated_future_value_message(equivalency, period)
-        CALCULATED_VALUE_MESSAGE % {
+      def calculated_future_value_with_message(equivalency, period)
+        InvestmentMessagesBuilderOptions.calculated_value_message % {
           current_value_type: :present,
           current_value: @investment.present_value,
           rate: equivalency_rounded_rate(equivalency.transformed_rate),
           period_quantity: period,
           period: equivalency_period(equivalency.class),
+          verb: 'returns',
           value_to_reach_type: :future,
           value_to_reach: @investment.calculated_future_value(equivalency, period)
         }
       rescue StonehengeBank::Resources::Investment::UncalculableInvestmentValueError => e
-        ERROR_MESSAGE % { error: e.to_s }
+        InvestmentMessagesBuilderOptions.error_message % { error: e.to_s }
+      end
+
+      def calculated_present_value_with_message(equivalency, period)
+        InvestmentMessagesBuilderOptions.calculated_value_message % {
+          current_value_type: :future,
+          current_value: @investment.future_value,
+          rate: equivalency_rounded_rate(equivalency.transformed_rate),
+          period_quantity: period,
+          period: equivalency_period(equivalency.class),
+          verb: 'has to have',
+          value_to_reach_type: :present,
+          value_to_reach: @investment.calculated_present_value(equivalency, period)
+        }
+      rescue StonehengeBank::Resources::Investment::UncalculableInvestmentValueError => e
+        InvestmentMessagesBuilderOptions.error_message % { error: e.to_s }
       end
 
       private
--- spec/stonehenge_bank/builders/investment_messages_builder_spec.rb
+++ spec/stonehenge_bank/builders/investment_messages_builder_spec.rb
@@ -10,11 +10,12 @@ module StonehengeBank
         it 'returns a formatted message for investment calculated future value' do
           allow(investment).to receive(:present_value).and_return 1000.0
           allow(equivalency).to receive(:transformed_rate).and_return 0.1248
+
           expect(investment).to receive(:calculated_future_value).with(equivalency, 2).
                                  and_return 1500.0
 
           expect(
-            subject.calculated_future_value_message(equivalency, 2)
+            subject.calculated_future_value_with_message(equivalency, 2)
           ).to eql "An investment with present value of $1000.0, an interest rate of 12.48 and a period of 2 year(s) returns a future value of $1500.0."
         end
 
@@ -26,7 +27,33 @@ module StonehengeBank
                                  and_raise(StonehengeBank::Resources::Investment::UncalculableInvestmentValueError, 'Foo')
 
           expect(
-            subject.calculated_future_value_message(equivalency, 2)
+            subject.calculated_future_value_with_message(equivalency, 2)
+          ).to eql "Cannot elaborate a message because of an error on the investment calculation: Foo. Please, check the values needed to be able to call the calculation again."
+        end
+      end
+
+      describe '#calculated_present_value_message equivalency, period' do
+        it 'returns a formatted message for investment calculated present value' do
+          allow(investment).to receive(:future_value).and_return(1000.0)
+          allow(equivalency).to receive(:transformed_rate).and_return 0.1248
+
+          expect(investment).to receive(:calculated_present_value).with(equivalency, 2).
+                                 and_return 740.0
+
+          expect(
+            subject.calculated_present_value_with_message(equivalency, 2)
+          ).to eql "An investment with future value of $1000.0, an interest rate of 12.48 and a period of 2 year(s) has to have a present value of $740.0."
+        end
+
+        it 'rescues investment errors and prompts a warning message' do
+          allow(investment).to receive(:future_value).and_return nil
+          allow(equivalency).to receive(:transformed_rate).and_return 0.1248
+
+          expect(investment).to receive(:calculated_present_value).with(equivalency, 2).
+                                 and_raise(StonehengeBank::Resources::Investment::UncalculableInvestmentValueError, 'Foo')
+
+          expect(
+            subject.calculated_present_value_with_message(equivalency, 2)
           ).to eql "Cannot elaborate a message because of an error on the investment calculation: Foo. Please, check the values needed to be able to call the calculation again."
         end
       end
