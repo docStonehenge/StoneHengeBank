164398acca5bb81bc8baeff1e32414bc55a55537
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-05-25 14:31:53 -0300

    Small refactorings on Investment

--- lib/stonehenge_bank/resources/investment.rb
+++ lib/stonehenge_bank/resources/investment.rb
@@ -12,41 +12,41 @@ module StonehengeBank
 
       def calculated_future_value(equivalency, period)
         raise UncalculableInvestmentValueError,
-              'Cannot calculate future value without a present value.' unless @present_value
+              'Cannot calculate future value without a present value.' unless present_value
 
-        (@present_value * period_power_calculated_rate(equivalency.transformed_rate, period)).round(2)
+        (present_value * period_power_calculated_rate(equivalency, period)).round(2)
       end
 
       def calculated_present_value(equivalency, period)
         raise UncalculableInvestmentValueError,
-              'Cannot calculate present value without a future value.' unless @future_value
+              'Cannot calculate present value without a future value.' unless future_value
 
-        (@future_value / period_power_calculated_rate(equivalency.transformed_rate, period)).round(2)
+        (future_value / period_power_calculated_rate(equivalency, period)).round(2)
       end
 
       def calculated_investment_period(equivalency)
         check_investment_values!(:period)
 
-        (Math.log(@future_value/@present_value) /
-          Math.log(neutralized_rate(equivalency.transformed_rate))).ceil
+        (Math.log(future_value/present_value) /
+          Math.log(neutralized_rate_for(equivalency))).ceil
       end
 
       def calculated_investment_rate(period_kind, quantity)
         check_investment_values!(:interest_rate)
         matches_real_period_kind?(period_kind)
 
-        ((((@future_value/@present_value)**(1/quantity.to_f)) - 1) * 100).round(2)
+        ((((future_value/present_value)**(1/quantity.to_f)) - 1) * 100).round(2)
       end
 
       def calculated_investment_regular_parcel(equivalency, period)
         raise UncalculableInvestmentValueError,
-              'Cannot calculate parcel without a future value.' unless @future_value
+              'Cannot calculate parcel without a future value.' unless future_value
 
         (
           future_value / (
-            neutralized_rate(equivalency.transformed_rate) * (
+            neutralized_rate_for(equivalency) * (
               (
-                (period_power_calculated_rate(equivalency.transformed_rate, period)) - 1
+                (period_power_calculated_rate(equivalency, period)) - 1
               ) / equivalency.transformed_rate
             )
           )
@@ -55,16 +55,16 @@ module StonehengeBank
 
       private
 
-      def neutralized_rate(rate)
-        1 + rate
+      def neutralized_rate_for(equivalency)
+        1 + equivalency.transformed_rate
       end
 
-      def period_power_calculated_rate(rate, period)
-        neutralized_rate(rate)**period
+      def period_power_calculated_rate(equivalency, period)
+        neutralized_rate_for(equivalency)**period
       end
 
       def check_investment_values!(value_type)
-        unless @present_value && @future_value
+        unless present_value && future_value
           raise UncalculableInvestmentValueError,
                 "Cannot calculate #{value_type.to_s.gsub('_', ' ')} with null values."
         end
