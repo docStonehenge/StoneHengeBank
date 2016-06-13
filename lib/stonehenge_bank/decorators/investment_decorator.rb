6adbb7644a51f74fc9a2d6b3cf0d1bd4d389f676
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-06-13 14:22:25 -0300

    Checks method name and if formatter exists before calls

--- lib/stonehenge_bank/decorators/investment_decorator.rb
+++ lib/stonehenge_bank/decorators/investment_decorator.rb
@@ -6,19 +6,31 @@ module StonehengeBank
       end
 
       def method_missing(method_name, *args)
-        if @investment.respond_to? method_name
-          @investment.public_send(method_name, *args)
-        else
+        if method_ends_with?('_with_message', method_name) and formatter_exists? method_name
           Formatters::BaseMessageFormatter.new(@investment).for_type(
             formatter_type_based_on_method_name(method_name)
           ).calculation_with_message(*args)
+        elsif method_ends_with?('_value', method_name) and @investment.respond_to? method_name
+          @investment.public_send(method_name, *args)
+        else
+          super
         end
       end
 
       private
 
+      def method_ends_with?(suffix, method_name)
+        method_name.to_s.end_with? suffix
+      end
+
       def formatter_type_based_on_method_name(method)
         method.to_s.match(/calculated_(\w+)_with/)[1]
+      end
+
+      def formatter_exists?(type)
+        Formatters.const_defined?(
+          "#{formatter_type_based_on_method_name(type).split('_').map(&:capitalize).join}MessageFormatter"
+        )
       end
     end
   end
--- spec/stonehenge_bank/decorators/investment_decorator_spec.rb
+++ spec/stonehenge_bank/decorators/investment_decorator_spec.rb
@@ -8,19 +8,49 @@ module StonehengeBank
       subject { described_class.new(investment) }
 
       describe '#method_missing method_name, *args' do
-        it 'calls method on investment if it responds to the message' do
-          expect(investment).to receive(:calculated_future_value).with(equivalency, 24)
-          subject.calculated_future_value(equivalency, 24)
+        context "when method name contains 'with_message' suffix" do
+          context 'when formatter exists' do
+            it 'instantiates the proper formatter and calls calculation on it' do
+              expect(Formatters).to receive(:const_defined?).
+                                     with('InvestmentRateMessageFormatter').and_return true
+
+              expect(
+                Formatters::BaseMessageFormatter
+              ).to receive(:new).with(investment).and_return(formatter)
+
+              expect(formatter).to receive(:for_type).with('investment_rate').and_return(formatter)
+              expect(formatter).to receive(:calculation_with_message).with(24)
+              subject.calculated_investment_rate_with_message(24)
+            end
+          end
+
+          context 'when formatter does not exist' do
+            it 'raises NoMethodError' do
+              expect(Formatters).to receive(:const_defined?).
+                                     with('FooMessageFormatter').and_return false
+
+              expect(
+                Formatters::BaseMessageFormatter
+              ).not_to receive(:new).with(any_args)
+
+              expect {
+                subject.calculated_foo_with_message(24)
+              }.to raise_error NoMethodError
+            end
+          end
         end
 
-        it 'instantiates the proper formatter and calls calculation on it' do
-          expect(
-            Formatters::BaseMessageFormatter
-          ).to receive(:new).with(investment).and_return(formatter)
+        context "when method name just contains '_value' suffix" do
+          it 'calls method on investment if it responds to the message' do
+            expect(investment).to receive(:calculated_future_value).with(equivalency, 24)
+            subject.calculated_future_value(equivalency, 24)
+          end
+        end
 
-          expect(formatter).to receive(:for_type).with('investment_rate').and_return(formatter)
-          expect(formatter).to receive(:calculation_with_message).with(24)
-          subject.calculated_investment_rate_with_message(24)
+        it 'raises NoMethodError for nonexistent methods on investment' do
+          expect {
+            subject.calculated_foo_value(equivalency, 2)
+          }.to raise_error(NoMethodError)
         end
       end
     end
