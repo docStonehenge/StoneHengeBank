7f4ca2c4b47ea0a75e454cf6b3c72505be844718
Victor Alexandre <victor.alexandrefs@gmail.com>
2016-09-25 13:11:42 -0300

    Performs calculation using a cash flow object

--- lib/stonehenge_bank/calculators/net_present_value.rb
+++ lib/stonehenge_bank/calculators/net_present_value.rb
@@ -1,20 +1,10 @@
 module StonehengeBank
   module Calculators
     class NetPresentValue
-      attr_reader :investments
-
-      def initialize(cost:)
-        @cost, @investments = cost, []
-      end
-
-      def append_investment(investment)
-        investments << investment
-      end
-
-      def calculate(using_equivalency:)
-        investments.each_with_index.inject(0) do |sum, (investment, index)|
+      def calculate(cash_flow, using_equivalency:)
+        cash_flow.investments.each_with_index.inject(0) do |sum, (investment, index)|
           sum += investment.calculated_present_value(using_equivalency, index+1)
-        end - @cost
+        end - cash_flow.cost
 
       rescue UncalculableInvestmentValueError
         raise CashFlowCalculationError,
--- spec/stonehenge_bank/calculators/net_present_value_spec.rb
+++ spec/stonehenge_bank/calculators/net_present_value_spec.rb
@@ -1,52 +1,33 @@
 module StonehengeBank
   module Calculators
     describe NetPresentValue do
-      let(:investment) { double(:investment) }
+      let(:cash_flow)                 { double(:cash_flow, cost: 3_000, investments: [investment_1, investment_2]) }
+      let(:investment_1)              { double(:investment) }
+      let(:investment_2)              { double(:investment) }
+      let(:year_interest_equivalency) { double(:year_interest_equivalency) }
 
-      subject { described_class.new(cost: 3_000) }
-
-      it { is_expected.to have_attributes(investments: []) }
-
-      describe '#append_investment investment' do
-        it 'appends an investment to hold its future value as a cash flow' do
-          subject.append_investment investment
-          expect(subject.investments).to include investment
-        end
-      end
-
-      describe '#calculate using_equivalency:' do
-        let(:investment_2)              { double(:investment) }
-        let(:investment_3)              { double(:investment) }
-        let(:year_interest_equivalency) { double(:year_interest_equivalency) }
-
-        it 'calculates the net present value of a project with determined cash flows' do
-          expect(subject).to receive(:investments).and_return [investment, investment_2, investment_3]
-          expect(investment).to receive(
-                                  :calculated_present_value
-                                ).with(year_interest_equivalency, 1).and_return 900.0
+      describe '#calculate cash_flow, using_equivalency:' do
+        it 'calculates the net present value of a project with determined cash flow' do
+          expect(investment_1).to receive(
+                                    :calculated_present_value
+                                  ).with(year_interest_equivalency, 1).and_return 1100.0
 
           expect(investment_2).to receive(
                                     :calculated_present_value
-                                  ).with(year_interest_equivalency, 2).and_return 1100.0
-
-          expect(investment_3).to receive(
-                                    :calculated_present_value
-                                  ).with(year_interest_equivalency, 3).and_return 1200.0
+                                  ).with(year_interest_equivalency, 2).and_return 1200.0
 
           expect(
-            subject.calculate(using_equivalency: year_interest_equivalency)
-          ).to eql 200.0
+            subject.calculate(cash_flow, using_equivalency: year_interest_equivalency)
+          ).to eql(-700.0)
         end
 
         it 'raises error when cash flow calculation runs on error' do
-          expect(subject).to receive(:investments).and_return [investment]
-
-          expect(investment).to receive(
+          expect(investment_1).to receive(
                                   :calculated_present_value
                                 ).with(year_interest_equivalency, 1).and_raise(UncalculableInvestmentValueError)
 
           expect {
-            subject.calculate(using_equivalency: year_interest_equivalency)
+            subject.calculate(cash_flow, using_equivalency: year_interest_equivalency)
           }.to raise_error(CashFlowCalculationError, 'An error occurred on Net Present Value calculation due to cash flow inconsistencies.')
         end
       end
