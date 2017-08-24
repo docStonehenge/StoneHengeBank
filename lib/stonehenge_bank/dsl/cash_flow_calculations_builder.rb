aca49c5bcb74944da699ba51632d204f96b5278a
Victor Alexandre <victor.alexandrefs@gmail.com>
2017-08-23 22:12:01 -0300

    Defines #simple_payback command on builder

--- lib/stonehenge_bank/dsl/cash_flow_calculations_builder.rb
+++ lib/stonehenge_bank/dsl/cash_flow_calculations_builder.rb
@@ -17,8 +17,10 @@ module StonehengeBank
       end
 
       def has_returns_of(*returns)
+        validate_cash_flow_instance_presence!
+
         returns.each do |return_value|
-          @cash_flow.append_investment_return(
+          cash_flow.append_investment_return(
             Resources::Investment.new(future_value: Float(return_value))
           )
         end
@@ -28,11 +30,25 @@ module StonehengeBank
       end
 
       def net_present_value
+        validate_cash_flow_instance_presence!
         raise ArgumentError, 'Interest rate equivalency is missing' unless equivalency
 
         cash_flow.net_present_value(
           Calculators::NetPresentValue.new, with_rate_equivalency: equivalency
         )
+      end
+
+      def simple_payback
+        validate_cash_flow_instance_presence!
+        cash_flow.payback_period(Calculators::Payback.simple)
+      end
+
+      private
+
+      def validate_cash_flow_instance_presence!
+        unless cash_flow
+          raise 'Cash flow calculation was not properly built: Cash flow instance is missing.'
+        end
       end
     end
   end
